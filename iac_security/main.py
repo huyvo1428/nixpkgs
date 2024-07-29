2e81a4d62b032167fe858da7edfaefd5c7fec6e1
Vo Quoc Huy <40543421+huyvo1428@users.noreply.github.com>
2024-07-29 23:11:16 +0900

    Update main.py

--- iac_security/main.py
+++ iac_security/main.py
@@ -28,11 +28,30 @@ def execute():
             no_code_smell_flag = 1
 
         if empty_default_result:
-            print(f"Detected admin by default in file {tf_file_path}")
+            print(f"Detected empty_default in file {tf_file_path}")
+            no_code_smell_flag = 1
+
+        if empty_password_result:
+            print(f"Detected empty_password in file {tf_file_path}")
+            no_code_smell_flag = 1
+
+        if harcoded_password_result:
+            no_code_smell_flag = 1
+
+        if inconsistent_naming_convention_result:
+            no_code_smell_flag = 1
+
+        if invalid_port_range_result:
+            no_code_smell_flag = 1
+
+        if long_statement_result:
+            no_code_smell_flag = 1
+
+        if unrestricted_ip_address_result:
             no_code_smell_flag = 1
 
     if no_code_smell_flag == 0:
-        print("No code smells detected")
+        print("0 code smells in your source code")
 
 
-cProfile.run('execute()')
+cProfile.run("execute()")
